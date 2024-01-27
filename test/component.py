60732dd4b7bd76a57a2c597dda0067f0dad0a1b6
Anton Shepitko <antonshepitko99@gmail.com>
2024-01-27 17:45:38 +0300

    test

--- test/component.py
+++ test/component.py
@@ -13,11 +13,11 @@ show_data = {
     "theater_id": 4
 }
 
-r = requests.get(f"{theater_url}/healthCheck", verify=False)
-r = requests.get(f"{tickets_url}/healthCheck", verify=False)
 
 def test_show_get():
     pytest.assume(requests.post(add_show_url, json=show_data) == 200)
     res = requests.get(f"{get_show_by_id_url}/{show_data['id']}")
     pytest.assume('type' in res.keys())
     pytest.assume('theater_id' in res.keys())
+
+test_show_get()
