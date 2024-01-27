02504c9bcc6e04a814d12a5024ccab79b6996426
yaroslavshula <uchebnayaposhta@yandex.ru>
2024-01-27 18:51:04 +0300

    component test

--- test/component.py
+++ test/component.py
@@ -17,8 +17,6 @@ show_data = {
 def test_show_get():
     assert (requests.post(add_show_url, json=show_data) == 200)
     res = requests.get(f"{get_show_by_id_url}/{show_data['id']}")
-    # pytest.assume('type' in res.keys())
-    # pytest.assume('theater_id' in res.keys())
     assert ('type' in res.keys())
 
 test_show_get()
